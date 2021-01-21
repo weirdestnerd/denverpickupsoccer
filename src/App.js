0dd847d1c5bed9617aec6b2d97249e77bb121cc1
Martin Naparstek <tinchonapa@gmail.com>
2021-01-20 23:19:43 -0700

    Added style to footer to accomodate fb group link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Logo from './logo.png'
+import Logo from './logo.png';
+import fb from './fb_icon.svg';
 
 const Navbar = () => {
   return <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -62,14 +63,20 @@ const PickupCard = ({ field, day, time, address, contact }) => {
 }
 
 const Footer = () => {
-    return <footer className='footer'>
-        <div className='has-text-centered'>
+    return <footer className='footer level'>
+        <div className='level-item has-text-centered'>
             <p>
                 <strong>Made by </strong>
                 <a href='https://linkedin.com/in/oludavid' target='_blank' rel="noopener noreferrer nofollow">
                     <span className='has-text-success'>Olu</span>
                 </a>
             </p>
+        </div>
+        <div className='level-item has-text-centered'>
+            <a href="https://www.facebook.com/groups/denverpickupsoccer/?__cft__[0]=AZV9jHuReRSAM-1Lkx50LDVO_vG2l3k4ietNdFSJGJEmQDOV6U3uSZRlmobsUQjOCaFj9_BjxbtXM1K734fDT5Us8DyZcjgJ1U3c17g8q2lIkvT2qyTjvW1K7Vx-ecMPk2kfttwQQiG3W_qYIRz7i_LXD5Lb9Z5pqWFbktUGjmA7o_2hCXHrYW3ThbmWv_IkOOA&amp;__tn__=-UC%2CP-R">
+                For More Games Follow: Denver Pick-Up Soccer 
+                <img className="svg_icon" src={fb} alt='Facbook Icon' />
+            </a>
         </div>
     </footer>
 }
