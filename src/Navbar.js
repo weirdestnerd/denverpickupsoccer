5b6b8296083f0b3fa273b5086f276e1342ad7e6b
Martin Naparstek <tinchonapa@gmail.com>
2021-01-29 22:14:36 -0700

    Separated code into components

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import Logo from './logo.png';
+
+const Navbar = () => {
+  return <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="/#">
+        <img src={Logo}
+             alt="logo" width="150"
+             height="30" />
+      </a>
+    </div>
+  </nav>
+}
+
+export default Navbar;
