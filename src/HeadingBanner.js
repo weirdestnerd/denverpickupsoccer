615f23781bad78b9fcda3fea0d73089affbc978c
Martin Naparstek <tinchonapa@gmail.com>
2021-01-31 12:20:39 -0700

    Deleted files that were moved into the components folder

--- src/HeadingBanner.js
+++
@@ -1,5 +0,0 @@
-const HeadingBanner = ({ text }) => {
-  return <h1 className="is-size-2 is-size-4-mobile has-background-white-ter has-text-left p-4 m-1 is-capitalized">{text}</h1>
-}
-
-export default HeadingBanner;
