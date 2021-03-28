f2b8cc0daca77160a3962bf7ff77c4f24ae2c1db
Olu D Gbadebo <odgbadebo@gmail.com>
2021-03-27 20:10:35 -0500

    added call to action in navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,4 +1,6 @@
-import Logo from '../logo.png';
+import React from 'react';
+import Logo from '../../logo.png';
+import './navbar.css'
 
 const Navbar = () => {
   return <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -7,6 +9,11 @@ const Navbar = () => {
         <img src={Logo}
              alt="logo" width="150"
              height="30" />
+      </a>
+    </div>
+    <div className="navbar-actions">
+      <a className="button is-primary mr-2 mt-2" href="/login">
+        Post your pickup
       </a>
     </div>
   </nav>
